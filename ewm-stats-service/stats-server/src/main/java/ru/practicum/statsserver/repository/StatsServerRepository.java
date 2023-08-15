f15131f0395bc36e2ba19e840485817d70833f71
Marat <marrrat1991@yandex.ru>
2023-08-16 00:20:37 +0500

    fix-9

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/statsserver/repository/StatsServerRepository.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/statsserver/repository/StatsServerRepository.java
@@ -11,18 +11,18 @@ import java.util.List;
 public interface StatsServerRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query("SELECT new ru.practicum.statsdto.ViewStatsDto(eh.app, eh.uri, COUNT (DISTINCT eh.ip)) FROM EndpointHit AS eh " +
-            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) AND (eh.uri IN (?3)) GROUP BY eh.uri")
+            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) AND (eh.uri IN (?3)) GROUP BY eh.uri ORDER BY COUNT (eh.ip) DESC")
     List<ViewStatsDto> findAllViewStatsWithUrisAndUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.statsdto.ViewStatsDto(eh.app, eh.uri, COUNT (eh.ip)) FROM EndpointHit AS eh " +
-            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) AND (eh.uri IN (?3)) GROUP BY eh.uri")
+            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) AND (eh.uri IN (?3)) GROUP BY eh.uri ORDER BY COUNT (eh.ip) DESC")
     List<ViewStatsDto> findAllViewStatsWithUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.statsdto.ViewStatsDto(eh.app, eh.uri, COUNT (eh.ip)) FROM EndpointHit AS eh " +
-            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) GROUP BY eh.uri")
+            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) GROUP BY eh.uri ORDER BY COUNT (eh.ip) DESC")
     List<ViewStatsDto> findAllViewStatsWithoutUris(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.statsdto.ViewStatsDto(eh.app, eh.uri, COUNT (DISTINCT eh.ip)) FROM EndpointHit AS eh " +
-            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) GROUP BY eh.uri")
+            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) GROUP BY eh.uri ORDER BY COUNT (eh.ip) DESC")
     List<ViewStatsDto> findAllViewStatsWithoutUrisAndUniqueIp(LocalDateTime start, LocalDateTime end);
 }
