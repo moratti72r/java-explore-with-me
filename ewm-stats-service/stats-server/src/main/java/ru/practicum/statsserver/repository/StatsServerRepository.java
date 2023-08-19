70be75c15c940c6a52a9d073f1836b9d3eb974be
Marat <marrrat1991@yandex.ru>
2023-08-19 18:05:58 +0500

    checkstyle

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/statsserver/repository/StatsServerRepository.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/statsserver/repository/StatsServerRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.statsserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.statsserver.model.EndpointHit;
 import ru.practicum.statsserver.model.ViewStats;
 
