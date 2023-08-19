b82f81f3e4674a1eb2aac23d6e4cdf4f555b6b75
Marat <marrrat1991@yandex.ru>
2023-08-19 18:03:26 +0500

    refactor: исправить ошибку класса StatsServerRepository

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/statsserver/repository/StatsServerRepository.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/statsserver/repository/StatsServerRepository.java
@@ -2,6 +2,7 @@ package ru.practicum.statsserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.statsserver.model.EndpointHit;
 import ru.practicum.statsserver.model.ViewStats;
 
@@ -11,10 +12,10 @@ import java.util.List;
 public interface StatsServerRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query("SELECT new ru.practicum.statsserver.model.ViewStats(eh.app, eh.uri, COUNT (DISTINCT eh.ip)) FROM EndpointHit AS eh " +
-            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) AND (?3 IS NULL OR eh.uri IN (?3)) GROUP BY eh.uri ORDER BY COUNT (eh.ip) DESC")
+            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) AND (eh.uri IN (?3) OR ?3 IS NULL) GROUP BY eh.uri ORDER BY COUNT (eh.ip) DESC")
     List<ViewStats> findAllViewStatsWithUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.statsserver.model.ViewStats(eh.app, eh.uri, COUNT (eh.ip)) FROM EndpointHit AS eh " +
-            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) AND (?3 IS NULL OR eh.uri IN (?3)) GROUP BY eh.uri ORDER BY COUNT (eh.ip) DESC")
+            "WHERE (eh.timestamp BETWEEN ?1 AND ?2) AND (eh.uri IN (?3) OR ?3 IS NULL) GROUP BY eh.uri ORDER BY COUNT (eh.ip) DESC")
     List<ViewStats> findAllViewStatsWithoutUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
